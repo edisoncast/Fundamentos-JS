2d27708785c598792d6a95b671f87f0448111832
edisoncast <edisoncastrosanchez@gmail.com>
2017-07-03 11:21:22 -0500

    Triangle Area using backtick

---
+++ Triangle.js
@@ -0,0 +1,12 @@
+let base = 5
+let height = 7
+
+function triangleArea(base,height){
+	return base * height / 2
+}
+
+//console.log("El área de un triágulo de base 5 y altura 7 es :" + 5*7/2)
+//console.log('El área de un triágulo de base 5 y altura 7 es :' + 5*7/2)
+console.log(`El área de un triágulo de base ${base} y altura ${height} es : ${triangleArea(base,height)}`)
+
+
